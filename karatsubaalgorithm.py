6beaa412c98910cb182a572ed862ac22d2ca9332
amir <amirali1690@gmail.com>
2019-12-09 21:26:53 -0700

    Karatsuba completed, Count inversion just created

--- karatsubaalgorithm.py
+++ karatsubaalgorithm.py
@@ -5,4 +5,20 @@ Created on Sat Dec  7 18:18:49 2019
 
 @author: amir
 """
+import math
 
+def karatsuba(x,y):
+    if x<10 and y<10:
+        return x*y
+    else:
+        n=math.log(x,10)
+        nf=n//2+1
+        a=int(x/10**(nf))
+        b=int(x%10**(nf))
+        c=int(y/10**(nf))
+        d=int(y%10**(nf))
+        ac=karatsuba(a,c)
+        bd=karatsuba(b,d)
+        adbc=karatsuba(a,d)+karatsuba(b,c)
+        prod = ac * 10**(2*nf) + (adbc * 10**nf) + bd
+        return int(prod)
